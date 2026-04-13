1d6878692df129b1464bcabaf90dadc097a38769
100ravthakurr <114392366+100ravthakurr@users.noreply.github.com>
2026-04-13 06:54:00 +0530

    iv

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef, useCallback } from "react";
 import "../header.css";
-import bgImage from "../assets/hero-bg.png"; // ← your manga silhouette image
+import bgImage from "../../public/image/trio.jpg"; // ← your manga silhouette image
 
 /* ─────────────────────────────────────────
    NAV LINKS
